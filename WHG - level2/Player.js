a38cb777b667e9e011512f0bdbe395fe84cc6476
Code Bullet <36225427+Code-Bullet@users.noreply.github.com>
2018-07-19 18:26:04 +1000

    Add files via upload

---
+++ WHG - level2/Player.js
@@ -0,0 +1,137 @@
+class Player{
+  constructor(){
+    this.pos = createVector(3*tileSize + xoff,4* tileSize + yoff);
+    this.vel = createVector(0,0);
+    this.size = tileSize/2.0;
+    this.playerSpeed = tileSize/15.0;
+    this.dead = false;
+    this.reachedGoal = false;
+    this.fadeCounter = 255;
+    this.isBest = false;
+    this.deathByDot = false;
+    this.deathAtStep = 0;
+    this.moveCount = 0;
+    this.gen =1;
+    this.fitness = 0;
+    this.nodes = [];
+    this.fading = false;
+    this.brain = new Brain(numberOfSteps);
+    this.human = false;
+    this.coin = new Coin(11*tileSize+xoff, 5*tileSize+yoff);
+    this.setNodes();
+
+  }
+
+  setNodes() {
+    this.nodes[1] = new Node(tiles[19][5],true,false);
+    this.nodes[0] = new Node(this.coin,false,true);
+    this.nodes[0].setDistanceToFinish(this.nodes[1]);
+  }
+
+  show(){
+    fill(255, 0, 0, this.fadeCounter);
+    if (this.isBest && !showBest) {
+      fill(0, 255, 0, this.fadeCounter);
+    }
+    stroke(0, 0, 0, this.fadeCounter);
+    strokeWeight(4);
+    rect(this.pos.x, this.pos.y, this.size, this.size);
+    stroke(0);
+    this.coin.show();
+  }
+
+  move(){
+    if (!humanPlaying){
+      if (this.moveCount == 0) {//move in the direction for 6 frames
+        if (this.brain.directions.length > this.brain.step) {//if there are still directions left then set the velocity as the next PVector in the direcitons array
+          this.vel = this.brain.directions[this.brain.step];
+          this.brain.step++;
+        } else {//if at the end of the directions array then the player is dead
+          this.dead = true;
+          this.fading = true;
+        }
+        this.moveCount =6;
+      } else {
+        this.moveCount--;
+      }
+    }
+    var temp = createVector(this.vel.x, this.vel.y);
+    temp.normalize();
+    temp.mult(this.playerSpeed);
+    for (var i = 0; i< solids.length; i++) {
+      temp = solids[i].restrictMovement(this.pos, createVector(this.pos.x+this.size, this.pos.y+this.size), temp);
+    }
+    this.pos.add(temp);
+
+  }
+
+  //checks if the player
+checkCollisions() {
+  this.coin.collides(this.pos, createVector(this.pos.x+this.size, this.pos.y+this.size));
+  for (var i = 0; i< dots.length; i++) {
+    if (dots[i].collides(this.pos, createVector(this.pos.x+this.size, this.pos.y+this.size))) {
+      this.fading = true;
+      this.dead = true;
+      this.deathByDot = true;
+      this.deathAtStep = this.brain.step;
+    }
+  }
+  if (this.coin.taken && winArea.collision(this.pos, createVector(this.pos.x+this.size, this.pos.y+this.size))) {
+    this.reachedGoal = true;
+  }
+  for (var i = 0; i< this.nodes.length; i++) {
+    this.nodes[i].collision(this.pos, createVector(this.pos.x+this.size, this.pos.y+this.size));
+  }
+}
+//----------------------------------------------------------------------------------------------------------------------------------------------------------
+ update() {
+  if (!this.dead && !this.reachedGoal) {
+    this.move();
+    this.checkCollisions();
+  } else if (this.fading) {
+    if (this.fadeCounter > 0) {
+      if(humanPlaying || replayGens){
+      this.fadeCounter -=10;
+    }else{
+      this.fadeCounter = 0;
+
+    }
+    }
+  }
+}
+//----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+ calculateFitness() {
+  if (this.reachedGoal) {//if the dot reached the goal then the fitness is based on the amount of steps it took to get there
+    this.fitness = 1.0/16.0 + 10000.0/(this.brain.step * this.brain.step);
+  } else {//if the dot didn't reach the goal then the fitness is based on how close it is to the goal
+    var estimatedDistance = 0.0;//the estimated distance of the path from the player to the goal
+    for (var i = this.nodes.length-1; i>=0; i--) {
+      if (!this.nodes[i].reached) {
+        estimatedDistance = this.nodes[i].distToFinish;
+        estimatedDistance += dist(this.pos.x, this.pos.y, this.nodes[i].pos.x, this.nodes[i].pos.y);
+      }
+    }
+    if (this.deathByDot) {
+      estimatedDistance *= 0.9;
+    }
+    this.fitness = 1.0/(estimatedDistance * estimatedDistance);
+  }
+  this.fitness*=this.fitness;
+  if(this.coin.taken){
+    this.fitness *=1.2;
+  }
+}
+
+
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------------
+ gimmeBaby() {
+  var baby = new Player();
+  baby.brain = this.brain.clone();//babies have the same brain as their parents
+  baby.deathByDot = this.deathByDot;
+  baby.deathAtStep = this.deathAtStep;
+  baby.gen = this.gen;
+  return baby;
+}
+}
---
+++ WHG - level2/setting_level.js
@@ -0,0 +1,211 @@
+//functions which setup the level
+
+function setLevel1Walls() {
+  tiles[0][0].wall = true;
+   tiles[0][1].wall = true;
+   tiles[0][2].wall = true;
+   tiles[0][3].wall = true;
+   tiles[0][4].wall = true;
+   tiles[0][5].wall = true;
+   tiles[0][6].wall = true;
+   tiles[0][7].wall = true;
+   tiles[0][8].wall = true;
+   tiles[0][9].wall = true;
+   tiles[1][0].wall = true;
+   tiles[1][1].wall = true;
+   tiles[1][2].wall = true;
+   tiles[1][3].wall = true;
+   tiles[1][4].wall = true;
+   tiles[1][5].wall = true;
+   tiles[1][6].wall = true;
+   tiles[1][7].wall = true;
+   tiles[1][8].wall = true;
+   tiles[1][9].wall = true;
+   tiles[2][0].wall = true;
+   tiles[2][1].wall = true;
+   tiles[2][2].wall = true;
+   tiles[2][3].wall = true;
+   tiles[2][6].wall = true;
+   tiles[2][7].wall = true;
+   tiles[2][8].wall = true;
+   tiles[2][9].wall = true;
+   tiles[3][0].wall = true;
+   tiles[3][1].wall = true;
+   tiles[3][2].wall = true;
+   tiles[3][3].wall = true;
+   tiles[3][6].wall = true;
+   tiles[3][7].wall = true;
+   tiles[3][8].wall = true;
+   tiles[3][9].wall = true;
+   tiles[4][0].wall = true;
+   tiles[4][1].wall = true;
+   tiles[4][2].wall = true;
+   tiles[4][3].wall = true;
+   tiles[4][6].wall = true;
+   tiles[4][7].wall = true;
+   tiles[4][8].wall = true;
+   tiles[4][9].wall = true;
+   tiles[5][0].wall = true;
+   tiles[5][1].wall = true;
+   tiles[5][8].wall = true;
+   tiles[5][9].wall = true;
+   tiles[6][0].wall = true;
+   tiles[6][1].wall = true;
+   tiles[6][8].wall = true;
+   tiles[6][9].wall = true;
+   tiles[7][0].wall = true;
+   tiles[7][1].wall = true;
+   tiles[7][8].wall = true;
+   tiles[7][9].wall = true;
+   tiles[8][0].wall = true;
+   tiles[8][1].wall = true;
+   tiles[8][8].wall = true;
+   tiles[8][9].wall = true;
+   tiles[9][0].wall = true;
+   tiles[9][1].wall = true;
+   tiles[9][8].wall = true;
+   tiles[9][9].wall = true;
+   tiles[10][0].wall = true;
+   tiles[10][1].wall = true;
+   tiles[10][8].wall = true;
+   tiles[10][9].wall = true;
+   tiles[11][0].wall = true;
+   tiles[11][1].wall = true;
+   tiles[11][8].wall = true;
+   tiles[11][9].wall = true;
+   tiles[12][0].wall = true;
+   tiles[12][1].wall = true;
+   tiles[12][8].wall = true;
+   tiles[12][9].wall = true;
+   tiles[13][0].wall = true;
+   tiles[13][1].wall = true;
+   tiles[13][8].wall = true;
+   tiles[13][9].wall = true;
+   tiles[14][0].wall = true;
+   tiles[14][1].wall = true;
+   tiles[14][8].wall = true;
+   tiles[14][9].wall = true;
+   tiles[15][0].wall = true;
+   tiles[15][1].wall = true;
+   tiles[15][8].wall = true;
+   tiles[15][9].wall = true;
+   tiles[16][0].wall = true;
+   tiles[16][1].wall = true;
+   tiles[16][8].wall = true;
+   tiles[16][9].wall = true;
+   tiles[17][0].wall = true;
+   tiles[17][1].wall = true;
+   tiles[17][2].wall = true;
+   tiles[17][3].wall = true;
+   tiles[17][6].wall = true;
+   tiles[17][7].wall = true;
+   tiles[17][8].wall = true;
+   tiles[17][9].wall = true;
+   tiles[18][0].wall = true;
+   tiles[18][1].wall = true;
+   tiles[18][2].wall = true;
+   tiles[18][3].wall = true;
+   tiles[18][6].wall = true;
+   tiles[18][7].wall = true;
+   tiles[18][8].wall = true;
+   tiles[18][9].wall = true;
+   tiles[19][0].wall = true;
+   tiles[19][1].wall = true;
+   tiles[19][2].wall = true;
+   tiles[19][3].wall = true;
+   tiles[19][6].wall = true;
+   tiles[19][7].wall = true;
+   tiles[19][8].wall = true;
+   tiles[19][9].wall = true;
+   tiles[20][0].wall = true;
+   tiles[20][1].wall = true;
+   tiles[20][2].wall = true;
+   tiles[20][3].wall = true;
+   tiles[20][4].wall = true;
+   tiles[20][5].wall = true;
+   tiles[20][6].wall = true;
+   tiles[20][7].wall = true;
+   tiles[20][8].wall = true;
+   tiles[20][9].wall = true;
+   tiles[21][0].wall = true;
+   tiles[21][1].wall = true;
+   tiles[21][2].wall = true;
+   tiles[21][3].wall = true;
+   tiles[21][4].wall = true;
+   tiles[21][5].wall = true;
+   tiles[21][6].wall = true;
+   tiles[21][7].wall = true;
+   tiles[21][8].wall = true;
+   tiles[21][9].wall = true;
+
+}
+
+
+function setLevel2Goal() {
+  tiles[17][4].goal = true;
+  tiles[17][5].goal = true;
+  tiles[18][4].goal = true;
+  tiles[18][5].goal = true;
+  tiles[19][4].goal = true;
+  tiles[19][5].goal = true;
+}
+
+function setLevel1SafeArea() {
+  tiles[2][4].safe = true;
+   tiles[2][5].safe = true;
+   tiles[3][4].safe = true;
+   tiles[3][5].safe = true;
+   tiles[4][4].safe = true;
+   tiles[4][5].safe = true;
+}
+
+
+function setEdges() {
+  for (var i = 1; i< tiles.length-1; i++) {
+    for (var j = 1; j< tiles[0].length-1; j++) {
+      if (tiles[i][j].wall) {
+        if (!tiles[i+1][j].wall) {
+          tiles[i][j].edges.push(1);
+        }
+        if (!tiles[i][j+1].wall) {
+          tiles[i][j].edges.push(2);
+        }
+        if (!tiles[i-1][j].wall) {
+          tiles[i][j].edges.push(3);
+        }
+        if (!tiles[i][j-1].wall) {
+          tiles[i][j].edges.push(4);
+        }
+      }
+    }
+  }
+}
+
+
+function setDots() {
+
+   dots.push(new Dot(tiles[5][2],tiles[5][7], 0, 1));
+   dots.push(new Dot(tiles[6][7],tiles[6][2], 0, -1));
+   dots.push(new Dot(tiles[7][2],tiles[7][7], 0, 1));
+   dots.push(new Dot(tiles[8][7],tiles[8][2], 0, -1));
+   dots.push(new Dot(tiles[9][2],tiles[9][7], 0, 1));
+   dots.push(new Dot(tiles[10][7],tiles[10][2], 0, -1));
+   dots.push(new Dot(tiles[11][2],tiles[11][7], 0, 1));
+   dots.push(new Dot(tiles[12][7],tiles[12][2], 0, -1));
+   dots.push(new Dot(tiles[13][2],tiles[13][7], 0, 1));
+   dots.push(new Dot(tiles[14][7],tiles[14][2], 0, -1));
+   dots.push(new Dot(tiles[15][2],tiles[15][7], 0, 1));
+   dots.push(new Dot(tiles[16][7],tiles[16][2], 0, -1));
+}
+
+
+function setSolids() {
+  solids.push(new Solid(tiles[1][6],tiles[4][8]));
+   solids.push(new Solid(tiles[4][8],tiles[17][8]));
+   solids.push(new Solid(tiles[17][6],tiles[20][8]));
+   solids.push(new Solid(tiles[20][3],tiles[20][6]));
+   solids.push(new Solid(tiles[17][1],tiles[20][3]));
+   solids.push(new Solid(tiles[4][1],tiles[17][1]));
+   solids.push(new Solid(tiles[1][1],tiles[4][3]));
+   solids.push(new Solid(tiles[1][3],tiles[1][6]));
+}
